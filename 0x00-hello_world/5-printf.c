1e9f586cb9ae2b60a152ca3d3cb39c7f03ac60a0
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-05 13:20:06 -0700

    C hello world task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - contains the entire function
+ * Return: returns 0 when the function has run
+ */
+
+int main(void)
+{
+	printf("with propar grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
