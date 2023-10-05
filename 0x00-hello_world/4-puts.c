7e4441de12110e38f01753b7f2060fa86cef12d5
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-05 11:23:17 -0700

    C hello world task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - contains the entire function
+ * Return: return a value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
+
