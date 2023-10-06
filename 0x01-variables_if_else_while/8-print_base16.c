712632753f5a9e5cabd30abb923dfc679750dfcf
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-06 14:46:48 -0700

    Variable task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - contains the entire program
+ *
+ * Return: returns 0 when the program has run
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	for (j = 0 ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
