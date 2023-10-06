461be0c830875e4b1424fe7734366cdbfe072f5c
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-06 12:09:46 -0700

    Variables task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - contains the entire program
+ * Return: returns 0 when the program has run
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
