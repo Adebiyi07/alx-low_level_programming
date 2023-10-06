7cc5ea47f09098f551a99fa9e207198c58276aa5
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-06 14:01:25 -0700

    Variables task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - contains the entire function
+ *
+ * Return: returns 0 when the program has run
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j <= 9 ; j++)
+		printf("%d", j);
+	printf("\n");
+	return (0);
+}
