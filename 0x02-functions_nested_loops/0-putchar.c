4efbf866e8e4b40b0251abcf96f601eb7562a9d0
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-10 13:29:43 -0700

    Nested loops task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,23 @@
-#include
+#include "main.h"
+
+/**
+ * main - contains the putchar function
+ *
+ * Return: return 0 after program has run
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+}
