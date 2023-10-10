7e8ac295c435aac659645e1aac354281313779e7
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-10 13:55:28 -0700

    Nested loops task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - contains the entire program to print alphabet
+ *
+ * Return: returns 0 after program runs
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+
+}
