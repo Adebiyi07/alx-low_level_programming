5d85eb4e9a1f3cd6bd35e31aae2167559a076ee7
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-10 19:07:41 -0700

    Nested loops task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function to sum numbers
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: sum from additionb a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
