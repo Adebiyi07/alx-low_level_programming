485dc1c1291a13ab55d1076151b1ab175f51b3b4
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-13 10:18:00 -0700

    More nested loops task7

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_triangle - fucntion to print triangles using #
+ *
+ * @size: parameter for size of triangle
+ *
+ * Return: returns 0
+ */
+
+void print_triangle(int size)
+{
+
+}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,23 @@
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		int i;
-		int j;
 
-		for (i = 0 ; i < n ; i++)
-		{
+	int i;
+	int j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (n > 1)
 			for (j = 0 ; j <= i ; j++)
+			{
 				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
+			}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
 	}
 }
+
