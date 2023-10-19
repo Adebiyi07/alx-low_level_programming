5edd9dc84b70db9556962dc2fba416c9a2a2e516
Adebiyi07 <adebiyijohn07@gmail.com>
2023-10-19 06:44:58 -0700

    More pointers task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = strlen(dest);
+	int len;
 	int i;
 
-	for (i = 0; i < n && *src != '\0'; i++)
+	for (len = 0; dest[len] != '\0'; len++)
+	{
+
+	}
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[len + i] = src[i];
-		src++;
 	}
-	dest[len + i] = '\0';
 	return (dest);
 }
