b98bc82d22d1e0cc09df578c3ba694227cfc3b6c
Adebiyi07 <adebiyijohn07@gmail.com>
2023-11-02 21:37:56 -0700

    More malloc task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of arrays
+ * @size: size of array
+ * Return: pointer to memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	output = calloc(nmemb, size);
+	if (output == NULL)
+		return (NULL);
+	else
+		return (output);
+}
